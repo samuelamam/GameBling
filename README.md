6ab9c4bfe0f290248650507fec0c7640d6fa38bf
Samuel <lameric1102@gmail.com>
2017-10-08 01:37:26 +0800

    初始化
    初始化

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# GameBling
+PHP7+NGINX
+
+在nginx vhost下添加伪静态（调试模式）
+
+location / {
+    index  index.html index.htm index.php;
+    if (-f $request_filename) {
+        expires max;
+        break;
+    }
+    if (!-e $request_filename) {
+        rewrite ^/(.*)$ /index.php/$1 last;
+    }
+    #autoindex  on;
+}
---
+++ application/views/admin/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="zh">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>后台管理系统</title>
+  <!-- Bootstrap core CSS-->
+  <link href="/application/views/admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="/application/views/admin/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Custom styles for this template-->
+  <link href="/application/views/admin/css/sb-admin.css" rel="stylesheet">
+</head>
+
+<body class="bg-dark">
+  <div class="container">
+    <div class="card card-login mx-auto mt-5">
+      <div class="card-header">登陆</div>
+      <div class="card-body">
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">用户名</label>
+            <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="请输入用户名">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">密码</label>
+            <input class="form-control" id="exampleInputPassword1" type="password" placeholder="请输入密码">
+          </div>
+          <!-- <div class="form-group">
+            <div class="form-check">
+              <label class="form-check-label">
+                <input class="form-check-input" type="checkbox"> Remember Password</label>
+            </div>
+          </div> -->
+          <a class="btn btn-primary btn-block" href="index.html">登陆</a>
+        </form>
+        <!-- <div class="text-center">
+          <a class="d-block small mt-3" href="register.html">Register an Account</a>
+          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+        </div> -->
+      </div>
+    </div>
+  </div>
+  <!-- Bootstrap core JavaScript-->
+  <script src="/application/views/admin/vendor/jquery/jquery.min.js"></script>
+  <script src="/application/views/admin/vendor/popper/popper.min.js"></script>
+  <script src="/application/views/admin/vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="/application/views/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+</body>
+
+</html>
