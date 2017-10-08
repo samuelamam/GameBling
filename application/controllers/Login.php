8c102d13794d9ad1877b2d35b8822d68e2e45fb0
heyki114 <405593392@qq.com>
2017-10-08 16:40:33 +0800

    add
    add

---
+++ application/controllers/Login.php
@@ -0,0 +1,84 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// class Login extends CI_Controller {
+class Login extends CI_Controller {
+
+	public function index(){
+		// print_r(123);exit;
+		$this->load->view('header');
+		$this->load->view('login');
+		$this->load->view('footer');
+
+	}
+
+	public function tijiao(){
+		$_where = array();
+	    $_like = array();
+	    $order_by = array();
+		$user = $this->input->post('user', TRUE);
+		$password = $this->input->post('password', TRUE);
+		$captcha = $this->input->post('captcha', TRUE);
+		$data = $this->db->query("SELECT passwd from sys_user WHERE `user` = 'admin'")->result();
+		print_r($data);
+		exit;
+		$total = $this->db
+		->select('password')
+		->from('sys_user')
+		->where('user',$user)
+		// ->like($_like)
+		->get()
+		->row_array();
+		// echo $total['password'];exit;
+		// session_unset();
+		// session_destroy();
+		// session_start();
+		if ($total['password'] == $password & $_SESSION['code'] == $captcha){
+			$_SESSION['user'] = $user;
+        	$_SESSION['password'] = $password;
+        	$lifeTime = 3600;
+        	setcookie(session_name(),session_id(),time() + $lifeTime,"/");
+        	// print_r($_SESSION);exit;
+        	// echo session_name();
+        	// echo session_id();exit;
+			// setcookie('user',$user,time()+3600,'/');
+			// setcookie('password',$password,time()+3600,'/');
+			echo "ok";
+		}else{
+			echo "error";
+		}
+
+		// print_r($this->db->last_query());exit;
+
+	}
+
+	function getCode($num=4,$w=60,$h=31) {
+		// 全数字
+		$str = "1,2,3,4,5,6,7,8,9";      //要显示的字符，可自己进行增删
+		$list = explode(",", $str);
+		$cmax = count($list) - 1;
+		$verifyCode = '';
+		for ( $i=0; $i < 4; $i++ ){
+		      $randnum = mt_rand(0, $cmax);
+		      $verifyCode .= $list[$randnum];           //取出字符，组合成为我们要的验证码字符
+		}
+		$_SESSION['code'] = $verifyCode;        //将字符放入SESSION中
+
+		$im = imagecreate(58,28);    //生成图片
+		$black = imagecolorallocate($im, 0,0,0);     //此条及以下三条为设置的颜色
+		$white = imagecolorallocate($im, 255,255,255);
+		$gray = imagecolorallocate($im, 200,200,200);
+		$red = imagecolorallocate($im, 255, 0, 0);
+		imagefill($im,0,0,$gray);     //给图片填充颜色
+
+		//将验证码绘入图片
+		imagestring($im, 5, 10, 8, $verifyCode, $black);    //将验证码写入到图片中
+
+		//生成验证码图片
+		header("Content-type: image/png");
+		imagepng($im);
+		imagedestroy($im);
+
+	}
+
+}
---
+++ application/views/login.php
@@ -0,0 +1,161 @@
+<style>
+    body {
+    background-color: white;
+    /*background:url(/Image/login.jpg)no-repeat 0 center;*/
+}
+
+#loginbox {
+    margin-top: 30px;
+}
+
+#loginbox > div:first-child {        
+    padding-bottom: 10px;    
+}
+
+.iconmelon {
+    display: block;
+    margin: auto;
+}
+
+#form > div {
+    margin-bottom: 25px;
+}
+
+#form > div:last-child {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+.panel {    
+    background-color: transparent;
+}
+
+.panel-body {
+    padding-top: 30px;
+    background-color: rgba(2555,255,255,.3);
+}
+
+#particles {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    top: 0;                        
+    bottom: 0;
+    left: 0;
+    right: 0;
+    position: absolute;
+    z-index: -2;
+}
+
+.iconmelon,
+.im {
+  position: relative;
+  width: 150px;
+  height: 150px;
+  display: block;
+  fill: #525151;
+}
+
+.iconmelon:after,
+.im:after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
+<div class="container">    
+        
+    <div id="loginbox" class="col-sm-5 col-sm-offset-4"> 
+        
+        <div class="row">                
+            <div class="iconmelon">
+              <svg viewBox="0 0 32 32">
+                <g filter="">
+                  <use xlink:href="#git"></use>
+                </g>
+              </svg>
+            </div>
+        </div>
+        
+        <div class="panel panel-default" >
+            <div class="panel-heading">
+                <div class="panel-title text-center">Please sign in</div>
+            </div>     
+
+            <div class="panel-body" >
+
+                <form name="form" id="form" class="form-horizontal" enctype="multipart/form-data" method="POST">
+                   
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="user" type="text" class="form-control" name="user" value="" placeholder="登录账号">
+                    </div>
+
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input id="password" type="password" class="form-control" name="password" placeholder="密码">
+                    </div>                                                                  
+                    <div class="input-group">
+                        <!-- Button -->
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-barcode"></i></span>
+                        <input id="captcha" type="text" class="form-control" name="captcha" placeholder="验证码" style="width: 200px">
+
+                        <img id="code" src="Login/getCode" alt="看不清楚，换一张" style="cursor: pointer; vertical-align:middle;" onclick="javascript:newgdcode(this,this.src);" />
+                    </div>
+                    <div class="form-group">
+                        <!-- Button -->
+                        <div class="col-sm-12 controls">
+                            <a id="tijiao" class="btn btn-primary pull-right"><i class="glyphicon glyphicon-log-in"></i> Log in</a>
+                        </div>
+                    </div>
+                </form>     
+            </div>                     
+        </div>
+      <div id="errAlert" class="alert alert-success" style="margin:0 auto;display: none">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Error! </strong>完整填写账号和密码，请注意大小写。
+      </div>
+    </div>
+</div>
+
+<script>
+  $('#tijiao').on('click', function(){
+        var user = $('#user').val();
+        var password = $('#password').val();
+        var captcha = $('#captcha').val();
+        if (user.length==0||password.length==0||captcha.length==0)
+        {
+          $("#errAlert").fadeTo(300,1.5);
+        }else{
+            // alert(password);
+            $.post('Login/tijiao',{"user":user,"password":password,"captcha":captcha},function(result){
+              if (result=='ok') {
+              window.location.href='/';
+              }else{
+                $("#errAlert").fadeTo(200,1.5);
+              }
+            });
+        }
+  });
+
+  $('#captcha').bind('keyup', function(event) {
+    if (event.keyCode == "13") {
+      //回车执行查询
+      $('#tijiao').click();
+    }
+  });
+
+  $('#code').on('click', function(){
+    $.post('Login/getCode',{},function(result){});
+
+  });
+
+  function newgdcode(obj,url) { 
+    obj.src = url+ '?nowtime=' + new Date().getTime(); 
+//后面传递一个随机参数，否则在IE7和火狐下，不刷新图片 
+  } 
+
+</script>
