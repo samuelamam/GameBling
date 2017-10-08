01e470de0732f6ba13d9afd60d040a303e9cae54
heyki114 <405593392@qq.com>
2017-10-09 02:32:41 +0800

    修改Login.html css文件路径
    修改Login.html css文件路径

--- application/views/header.php
+++ application/views/header.php
@@ -8,74 +8,74 @@
     <meta name="description" content=""/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <!-- bootstrap & fontawesome -->
-    <link rel="stylesheet" href="resources/assets/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="resources/assets/css/font-awesome.min.css"/>
-    <link rel="stylesheet" href="resources/plugins/bootstrap-select/css/bootstrap-select.min.css"/>
+    <link rel="stylesheet" href="/resources/assets/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/resources/assets/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="/resources/plugins/bootstrap-select/css/bootstrap-select.min.css"/>
     <!-- page specific plugin styles -->
-    <link rel="stylesheet" href="resources/assets/css/jquery-ui.custom.min.css"/>
-    <link href="resources/assets/css/bootstrap-datetimepicker.css" rel="stylesheet">
-    <link rel="stylesheet" href="resources/assets/css/datepicker.css" />
-    <link rel="stylesheet" href="resources/assets/css/daterangepicker.css" />
+    <link rel="stylesheet" href="/resources/assets/css/jquery-ui.custom.min.css"/>
+    <link href="/resources/assets/css/bootstrap-datetimepicker.css" rel="stylesheet">
+    <link rel="stylesheet" href="/resources/assets/css/datepicker.css" />
+    <link rel="stylesheet" href="/resources/assets/css/daterangepicker.css" />
     <!-- text fonts -->
-    <link rel="stylesheet" href="resources/assets/css/ace-fonts.css"/>
+    <link rel="stylesheet" href="/resources/assets/css/ace-fonts.css"/>
 
     <!-- ace styles -->
-    <link rel="stylesheet" href="resources/assets/css/ace.min.css" id="main-ace-style"/>
-    <link rel="stylesheet" href="resources/plugins/mmGrid-master/mmGrid.css" id="main-ace-style"/>
-    <link rel="stylesheet" href="resources/plugins/mmGrid-master/theme/bootstrap/mmGrid-bootstrap.css" id="main-ace-style"/>
-    <link rel="stylesheet" href="resources/plugins/mmGrid-master/mmPaginator.css" id="main-ace-style"/>
-    <link rel="stylesheet" href="resources/plugins/mmGrid-master/theme/bootstrap/mmPaginator-bootstrap.css" id="main-ace-style"/>
-    <link rel="stylesheet" href="resources/plugins/input/input.css" id="main-ace-style"/>
-    <link rel="stylesheet" href="resources/plugins/bootstrap-dialog/css/bootstrap-dialog.css" id="main-ace-style"/>
-    <link rel="stylesheet" href="resources/plugins/fancytree/css/skin-win7/ui.fancytree.min.css" id="main-ace-style"/>
-    <link rel="stylesheet" href="resources/common/css/common.css"/>
+    <link rel="stylesheet" href="/resources/assets/css/ace.min.css" id="main-ace-style"/>
+    <link rel="stylesheet" href="/resources/plugins/mmGrid-master/mmGrid.css" id="main-ace-style"/>
+    <link rel="stylesheet" href="/resources/plugins/mmGrid-master/theme/bootstrap/mmGrid-bootstrap.css" id="main-ace-style"/>
+    <link rel="stylesheet" href="/resources/plugins/mmGrid-master/mmPaginator.css" id="main-ace-style"/>
+    <link rel="stylesheet" href="/resources/plugins/mmGrid-master/theme/bootstrap/mmPaginator-bootstrap.css" id="main-ace-style"/>
+    <link rel="stylesheet" href="/resources/plugins/input/input.css" id="main-ace-style"/>
+    <link rel="stylesheet" href="/resources/plugins/bootstrap-dialog/css/bootstrap-dialog.css" id="main-ace-style"/>
+    <link rel="stylesheet" href="/resources/plugins/fancytree/css/skin-win7/ui.fancytree.min.css" id="main-ace-style"/>
+    <link rel="stylesheet" href="/resources/common/css/common.css"/>
     <!-- inline styles related to this page -->
-    <link rel="stylesheet" href="resources/assets/css/chosen.css" id="main-ace-style"/>
+    <link rel="stylesheet" href="/resources/assets/css/chosen.css" id="main-ace-style"/>
     <!-- ace settings handler -->
-    <script src="resources/assets/js/ace-extra.min.js"></script>
+    <script src="/resources/assets/js/ace-extra.min.js"></script>
     <!-- basic scripts -->
 
     <!--[if !IE]> -->
     <script type="text/javascript">
-        window.jQuery || document.write("<script src='resources/assets/js/jquery.min.js'>" + "<" + "/script>");
+        window.jQuery || document.write("<script src='/resources/assets/js/jquery.min.js'>" + "<" + "/script>");
     </script>
 
     <!-- <![endif]-->
 
     <!--[if IE]>
     <script type="text/javascript">
-        window.jQuery || document.write("<script src='resources/assets/js/jquery1x.min.js'>" + "<" + "/script>");
+        window.jQuery || document.write("<script src='/resources/assets/js/jquery1x.min.js'>" + "<" + "/script>");
     </script>
     <![endif]-->
-    <script src="resources/plugins/bootstrap-select/js/bootstrap-select.min.js"></script>
-    <script src="resources/assets/js/bootstrap.min.js"></script>
+    <script src="/resources/plugins/bootstrap-select/js/bootstrap-select.min.js"></script>
+    <script src="/resources/assets/js/bootstrap.min.js"></script>
 
     <!-- page specific plugin scripts -->
-    <script src="resources/assets/js/jquery-ui.min.js"></script>
+    <script src="/resources/assets/js/jquery-ui.min.js"></script>
     <!-- ace scripts -->
-    <script src="resources/assets/js/ace-elements.min.js"></script>
-    <!--<script src="resources/assets/js/ace.min.js"></script>-->
-    <script src="resources/assets/js/uncompressed/ace.js"></script>
-    <script src="resources/assets/js/ace-extra.min.js"></script>
-    <script src="resources/plugins/mmGrid-master/mmGrid.js"></script>
-    <script src="resources/plugins/mmGrid-master/mmPaginator.js"></script>
-    <script src="resources/assets/js/uncompressed/date-time/bootstrap-datepicker.js"></script>
-    <script src="resources/assets/js/date-time/bootstrap-datepicker.min.js"></script>
-    <script src="resources/assets/js/date-time/bootstrap-timepicker.min.js"></script>
-    <script src="resources/assets/js/date-time/moment.min.js"></script>
-    <script src="resources/assets/js/date-time/daterangepicker.min.js"></script>
-    <script src="resources/assets/js/date-time/bootstrap-datetimepicker.min.js"></script>
-    <script src="resources/assets/js/date-time/daterangepicker.min.js"></script>
-    <script src="resources/assets/js/uncompressed/bootbox.js"></script>
-    <script src="resources/assets/js/jquery.validate.min.js"></script>
-    <script src="resources/plugins/input/input.js"></script>
-    <script src="resources/plugins/bootstrap-dialog/js/bootstrap-dialog.js"></script>
-    <script src="resources/plugins/fancytree/js/jquery.fancytree-all.min.js"></script>
-    <script src="resources/common/js/common.js"></script>
-    <script src="resources/assets/js/chosen.jquery.min.js"></script>
-    <script src="resources/assets/js/fuelux/fuelux.spinner.min.js"></script>
-    <script src="resources/plugins/form.js"></script>
-    <script src="resources/plugins/jquery.form.js"></script>
-    <script src="resources/assets/js/typeahead.jquery.min.js"></script>
-    <script src="resources/plugins/echarts/echarts-all.js"></script>
+    <script src="/resources/assets/js/ace-elements.min.js"></script>
+    <!--<script src="/resources/assets/js/ace.min.js"></script>-->
+    <script src="/resources/assets/js/uncompressed/ace.js"></script>
+    <script src="/resources/assets/js/ace-extra.min.js"></script>
+    <script src="/resources/plugins/mmGrid-master/mmGrid.js"></script>
+    <script src="/resources/plugins/mmGrid-master/mmPaginator.js"></script>
+    <script src="/resources/assets/js/uncompressed/date-time/bootstrap-datepicker.js"></script>
+    <script src="/resources/assets/js/date-time/bootstrap-datepicker.min.js"></script>
+    <script src="/resources/assets/js/date-time/bootstrap-timepicker.min.js"></script>
+    <script src="/resources/assets/js/date-time/moment.min.js"></script>
+    <script src="/resources/assets/js/date-time/daterangepicker.min.js"></script>
+    <script src="/resources/assets/js/date-time/bootstrap-datetimepicker.min.js"></script>
+    <script src="/resources/assets/js/date-time/daterangepicker.min.js"></script>
+    <script src="/resources/assets/js/uncompressed/bootbox.js"></script>
+    <script src="/resources/assets/js/jquery.validate.min.js"></script>
+    <script src="/resources/plugins/input/input.js"></script>
+    <script src="/resources/plugins/bootstrap-dialog/js/bootstrap-dialog.js"></script>
+    <script src="/resources/plugins/fancytree/js/jquery.fancytree-all.min.js"></script>
+    <script src="/resources/common/js/common.js"></script>
+    <script src="/resources/assets/js/chosen.jquery.min.js"></script>
+    <script src="/resources/assets/js/fuelux/fuelux.spinner.min.js"></script>
+    <script src="/resources/plugins/form.js"></script>
+    <script src="/resources/plugins/jquery.form.js"></script>
+    <script src="/resources/assets/js/typeahead.jquery.min.js"></script>
+    <script src="/resources/plugins/echarts/echarts-all.js"></script>
 </head>
